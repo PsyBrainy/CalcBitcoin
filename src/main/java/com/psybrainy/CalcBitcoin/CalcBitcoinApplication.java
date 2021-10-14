ed6d685c3c89f9500aa1f3fb5a427052ee4a13ce
psybrainy <martuto93@gmail.com>
2021-10-13 21:23:56 -0300

    init

---
+++ src/main/java/com/psybrainy/CalcBitcoin/CalcBitcoinApplication.java
@@ -0,0 +1,13 @@
+package com.psybrainy.CalcBitcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcBitcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcBitcoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psybrainy/CalcBitcoin/CalcBitcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybrainy.CalcBitcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcBitcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
