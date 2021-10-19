d25f4dfbe91f522703ae58c55303b6d74c9b3b58
psybrainy <martuto93@gmail.com>
2021-10-19 12:12:00 -0300

    se agrego metodo controlador y modifio dto y entidad

--- src/main/java/com/psybrainy/CalcBitcoin/persistence/entity/BitcoinMayorEntity.java
+++ src/main/java/com/psybrainy/CalcBitcoin/persistence/entity/BitcoinMayorEntity.java
@@ -1,12 +1,16 @@
 package com.psybrainy.CalcBitcoin.persistence.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "bitcoin_mayor_valor")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BitcoinMayorEntity {
 
     @Id
