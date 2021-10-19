11e4c062ff60e648f3dfb4c1641e3fdbd4928b43
psybrainy <martuto93@gmail.com>
2021-10-19 12:13:33 -0300

    se agrego logica del servicio y repositorio

--- src/main/java/com/psybrainy/CalcBitcoin/persistence/BitcoinRepository.java
+++ src/main/java/com/psybrainy/CalcBitcoin/persistence/BitcoinRepository.java
@@ -1,7 +1,12 @@
 package com.psybrainy.CalcBitcoin.persistence;
 
+import com.psybrainy.CalcBitcoin.persistence.entity.BitcoinMayorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BitcoinRepository {
+public interface BitcoinRepository extends JpaRepository<BitcoinMayorEntity, Long> {
+
+    BitcoinRepository findByValorMaximo(Double valorMaximo);
 }
--- src/main/java/com/psybrainy/CalcBitcoin/service/BitcoinService.java
+++ src/main/java/com/psybrainy/CalcBitcoin/service/BitcoinService.java
@@ -1,11 +1,14 @@
 package com.psybrainy.CalcBitcoin.service;
 
+import com.psybrainy.CalcBitcoin.dto.BitcoinCalcDto;
 import com.psybrainy.CalcBitcoin.dto.BitcoinDto;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public interface BitcoinService {
 
     Flux<BitcoinDto> getBitcoin();
+    Flux<BitcoinCalcDto> getCalc();
 }
